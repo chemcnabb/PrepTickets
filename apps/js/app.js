fb61d4cb4cf7b3db3604a1b299dc275459e80f28
Andrew Kalek <andrew.kalek@anlek.com>
2013-06-27 02:33:24 -0400

    Removed extra folders and am looking at refactoring it

--- apps/js/app.js
+++ apps/js/app.js
@@ -1,5 +1,15 @@
-'use strict';
+//= require BWL/base
+//= require angular
+//= require ui-bootstrap
+//= require jquery
+//= require jquery.validate
+//= require ui/jquery-ui
+//= require BWL/extensions
+//= require BWL/models
+//= require BWL/models.enums
+//= require BWL/models.meta
+//= require BWL/proxy
+//= require BWL/services
+//= require BWL/services.meta
 
-
-// Declare app level module which depends on filters, and services
-
+//= require application
